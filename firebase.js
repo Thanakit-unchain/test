c05564cf58cefc54a31c5d803f8885c798b65393
TG <gowitwanichthanakit@gmail.com>
2025-05-14 12:53:26 +0900

    js seperate

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getMessaging, onMessage } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-messaging.js";
+import dotenv from 'dotenv';
+dotenv.config()
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging(app);
+onMessage(messaging, (payload) => {
+    console.log('Message received. ', payload);
+    Notification.postMessage('Message received. ', payload);
+});
