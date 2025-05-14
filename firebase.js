38c798b7e58bd7734b85efe461e256213ac1099e
TG <gowitwanichthanakit@gmail.com>
2025-05-14 13:53:51 +0900

    a

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import { getMessaging, onMessage } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-messaging.js";
-
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
 
 export default function initFirebase() {
     dotenv.config()
 
     // Your web app's Firebase configuration
     const firebaseConfig = {
-        apiKey: process.env.APIKEY,
-        authDomain: process.env.AUTHDOMAIN,
-        projectId: process.env.PROJECTID,
-        storageBucket: process.env.STORAGEBUCKET,
-        messagingSenderId: process.env.MESSAGINGSENDERID,
-        appId: process.env.APPID
+        apiKey: "AIzaSyAj6lamU9O-hds-uelCbfiDVSUmSaLxvvc",
+        authDomain: "myproject-b40b9.firebaseapp.com",
+        projectId: "myproject-b40b9",
+        storageBucket: "myproject-b40b9.firebasestorage.app",
+        messagingSenderId: "887354787020",
+        appId: "1:887354787020:web:562186b4730490212f6e69"
     };
 
     // Initialize Firebase
