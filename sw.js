55f03cf028e40f612812aac310d5ead23fcb0454
TG <gowitwanichthanakit@gmail.com>
2025-05-14 15:39:55 +0900

    d

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+    apiKey: "AIzaSyAj6lamU9O-hds-uelCbfiDVSUmSaLxvvc",
+    authDomain: "myproject-b40b9.firebaseapp.com",
+    projectId: "myproject-b40b9",
+    storageBucket: "myproject-b40b9.firebasestorage.app",
+    messagingSenderId: "887354787020",
+    appId: "1:887354787020:web:562186b4730490212f6e69"
+});
+
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
     console.log(
